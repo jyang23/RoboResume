c1cee7b59c1a433cce6b7050d4ec3081356d014b
Justin <justinyang1000@hotmail.com>
2019-07-12 11:31:11 -0400

    Initial commit

---
+++ src/main/java/com/jy/roboresume/Resume.java
@@ -0,0 +1,84 @@
+package com.jy.roboresume;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Resume {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    private String emailaddress;
+
+    private String phonenumber;
+
+    private String education;
+
+    private String experience;
+
+    private String skills;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailaddress() {
+        return emailaddress;
+    }
+
+    public void setEmailaddress(String emailaddress) {
+        this.emailaddress = emailaddress;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public void setExperience(String experience) {
+        this.experience = experience;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+}
+
---
+++ src/main/java/com/jy/roboresume/RoboresumeApplication.java
@@ -0,0 +1,13 @@
+package com.jy.roboresume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboresumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoboresumeApplication.class, args);
+    }
+
+}
